b6c138b8f1cd94adbd1eaea1843b7eeb4a9b680e
Nicola B. Bernardelli <nicola.bernardelli@gmail.com>
2015-07-14 02:39:34 -0300

    prototyping coexistence of radar control with throttle control

--- Aircraft/JA37/Models/Cockpit/throttle/pngPathFix.sh
+++ Aircraft/JA37/Models/Cockpit/throttle/pngPathFix.sh
@@ -1,2 +1,2 @@
-sed 's/alu.png/\.\.\/alu.png/' throttle_fixme.ac | sed 's/black.png/\.\.\/black.png/' | sed 's/greenishMetal.png/\.\.\/greenishMetal.png/' | sed 's/blueishMetal.png/\.\.\/blueishMetal.png/' | sed 's/brass.png/\.\.\/brass.png/' | less -i > throttle.ac
+sed 's/alu.png/\.\.\/alu.png/' throttle_fixme.ac | sed 's/black.png/\.\.\/black.png/' | sed 's/greenishMetal.png/\.\.\/greenishMetal.png/' | sed 's/blueishMetal.png/\.\.\/blueishMetal.png/' | sed 's/brass.png/\.\.\/brass.png/' > throttle.ac
 
