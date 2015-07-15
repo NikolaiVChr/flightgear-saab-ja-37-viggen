2e8dd5b1f236e1dab15d28cad002778e5c984189
Nicola B. Bernardelli <nicola.bernardelli@gmail.com>
2015-07-15 04:11:16 -0300

    still prototyping

--- Aircraft/JA37/Models/Cockpit/radarControl/pngPathFix.sh
+++ Aircraft/JA37/Models/Cockpit/radarControl/pngPathFix.sh
@@ -1,4 +1,17 @@
 # sed 's/alu.png/\.\.\/alu.png/' throttle_fixme.ac | sed 's/black.png/\.\.\/black.png/' | sed 's/greenishMetal.png/\.\.\/greenishMetal.png/' | sed 's/blueishMetal.png/\.\.\/blueishMetal.png/' | sed 's/brass.png/\.\.\/brass.png/' | sed 's/rubberGrip_brown_665e5a.png.png/\.\.\/rubberGrip_brown_665e5a.png.png/' | less -i > throttle.ac
 
 # standard input
-sed 's/alu.png/\.\.\/alu.png/' | sed 's/black.png/\.\.\/black.png/' | sed 's/greenishMetal.png/\.\.\/greenishMetal.png/' | sed 's/blueishMetal.png/\.\.\/blueishMetal.png/' | sed 's/brass.png/\.\.\/brass.png/' | sed 's/rubberGrip_brown_665e5a.png/\.\.\/rubberGrip_brown_665e5a.png/' | sed 's/rubberGrip_black_1a1817.png/\.\.\/rubberGrip_black_1a1817.png/'
+# sed 's/alu.png/\.\.\/alu.png/' | sed 's/black.png/\.\.\/black.png/' | sed 's/greenishMetal.png/\.\.\/greenishMetal.png/' | sed 's/blueishMetal.png/\.\.\/blueishMetal.png/' | sed 's/brass.png/\.\.\/brass.png/' | sed 's/rubberGrip_brown_665e5a.png/\.\.\/rubberGrip_brown_665e5a.png/' | sed 's/rubberGrip_black_1a1817.png/\.\.\/rubberGrip_black_1a1817.png/'
+
+sed 's/alu.png/\.\.\/alu.png/' \
+ | sed 's/black.png/\.\.\/black.png/' \
+ | sed 's/greenishMetal.png/\.\.\/greenishMetal.png/' \
+ | sed 's/blueishMetal.png/\.\.\/blueishMetal.png/' \
+ | sed 's/brass.png/\.\.\/brass.png/' \
+ | sed 's/rubberGrip_brown_665e5a.png/\.\.\/rubberGrip_brown_665e5a.png/' \
+ | sed 's/rubberGrip_black_1a1817.png/\.\.\/rubberGrip_black_1a1817.png/' \
+ | sed 's/black_1a1817_rubberGrip.png/\.\.\/black_1a1817_rubberGrip.png/' \
+ | sed 's/black_hex191919_knurled_stronger.png/\.\.\/black_hex191919_knurled_stronger.png/' \
+ | sed 's/black_hex191919_uniform.png/\.\.\/black_hex191919_uniform.png/' \
+ | sed 's/black_hex191919_knurled_evenStronger.png/\.\.\/black_hex191919_knurled_evenStronger.png/'
+ 
