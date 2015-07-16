feab84b953a3ce373cbad2110d0dbdb4d4154e2f
Nicola B. Bernardelli <nicola.bernardelli@gmail.com>
2015-07-16 03:36:34 -0300

    Throttle and radar controls - moved radar switches - eventual further improvements will go into next release.

--- Aircraft/JA37/Models/Cockpit/radarControl/getListOfObjects.sh
+++
@@ -1,6 +0,0 @@
-# grep 'name \"' throttle.ac | sed 's/name\ \"//' | sed s/\"//
-# grep 'name \"' radarControl.ac | sed "s/name\ \"/\<object\-name\>/" | sed "s/\"/\<\/object\-name\>/"
-
-# standard input
-grep 'name \"' - | sed "s/name\ \"/\t\t\<object\-name\>/" | sed "s/\"/\<\/object\-name\>/"
-
