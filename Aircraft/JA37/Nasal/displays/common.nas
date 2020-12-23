NikolaiVChr/flightgear-saab-ja-37-viggen

commit 61b1dd84344963c5a99908e5018048521b23dd4e

AJS HUD: Implement ground corrected altitude
When switch HÖJD CISI is in position RHM, radar altitude is used to
compute a ground corrected altitude. It is displayed on the HUD, and is
used to compute target distance when aiming.

Aircraft/JA37/Nasal/displays/common.nas
@@ -72,9 +72,11 @@ var Common = {
 			wow1:             "fdm/jsbsim/gear/unit[1]/WOW",
 			nav0InRange:      "instrumentation/nav[0]/in-range",
 			qfeWarning:       "ja37/displays/qfe-warning",
-			alt_m:            "instrumentation/altimeter/indicated-altitude-meter",
+			alt_m:            "instrumentation/altimeter/displays-altitude-meter",
+			alt_bar_m:        "instrumentation/altimeter/indicated-altitude-meter",
 			altimeter_std:    "instrumentation/altimeter/setting-std",
 			ref_alt:          "ja37/displays/reference-altitude-m",
+			switch_hojd:      "ja37/hud/switch-hojd",
 			APmode:           "fdm/jsbsim/autoflight/mode",
 			AP_alt_ft:        "fdm/jsbsim/autoflight/pitch/alt/target",
 			units:            "ja37/hud/units-metric",
@@ -136,6 +138,7 @@ var Common = {
 		me.errors();
 		me.flighttime();
 		me.referenceAlt();
+		me.groundCorrectedAltitude();
 		me.EP13();
 		#me.rate = getprop("sim/frame-rate-worst");
 		#settimer(func me.loop(), me.rate!=nil?clamp(2.15/(me.rate+0.001), 0.05, 0.5):0.5);#0.001 is to prevent divide by zero
@@ -404,20 +407,20 @@ var Common = {
 		if (modes.takeoff) {
 			me.ref_alt = 500;
 			me.ref_alt_ldg_override = FALSE;
-		} elsif (modes.landing) {
+		} elsif (me.input.APmode.getValue() == 3) {
+			me.ref_alt = me.input.AP_alt_ft.getValue() * FT2M;
+			if (!variant.JA) {
+				# For AJS, autopilot uses barometric altitude, but displays use ground corrected altitude.
+				me.ref_alt += me.input.alt_m.getValue() - me.input.alt_bar_m.getValue();
+			}
+			me.ref_alt_ldg_override = FALSE;
+		} elsif (modes.landing and !me.ref_alt_ldg_override) {
 			# me.ref_alt_ldg_override indicates that the altitude was manually selected
 			# with the reference altitude button while in LANDING mode.
 			# This flag is cleared in every other mode, which resets the altitude to 500 when switching to LANDING.
-			if (me.input.APmode.getValue() == 3) {
-				me.ref_alt_ldg_override = FALSE;
-				me.ref_alt = me.input.AP_alt_ft.getValue() * FT2M;
-			} elsif (!me.ref_alt_ldg_override) {
-				me.ref_alt = 500;
-			}
+			me.ref_alt = 500;
 		} else {
-			if (me.input.APmode.getValue() == 3) {
-				me.ref_alt = me.input.AP_alt_ft.getValue() * FT2M;
-			}
+			# navigation mode
 			me.ref_alt_ldg_override = FALSE;
 		}
 		me.input.ref_alt.setValue(me.ref_alt);
@@ -425,7 +428,7 @@ var Common = {
 
 	refAltButton: func {
 		# For AJS, different functionality in LOW NAV (declutter) mode
-		if (getprop("/ja37/systems/variant") != 0 and hud.hud.mode == hud.hud.MODE_NAV_DECLUTTER) {
+		if (!variant.JA and hud.hud.mode == hud.hud.MODE_NAV_DECLUTTER) {
 			hud.hud.declutter_heading_toggle();
 			return;
 		}
@@ -453,6 +456,18 @@ var Common = {
 		);
 	},
 
+	# Logic for AJS switch HÖJD CI/SI (radar altitude correction mode).
+	# The ground corrected altitude is computed in JSBSim.
+	groundCorrectedAltitude: func {
+		if (me.input.switch_hojd.getBoolValue()
+			# Should be standard pressure altitude, but it would make it unusable in high terrain.
+			and (me.input.alt_bar_m.getValue() > 2450
+				or (modes.landing and land.mode >= 2 and land.mode <= 3))) {
+			me.input.switch_hojd.setValue(FALSE);
+			ja37.click();
+		}
+	},
+
 	setCursorDisplay: func (display) {
 		me.cursor = display;
 		me.resetCursorDelta();
@@ -473,6 +488,7 @@ var Common = {
 		me.input.cursor_dy.setValue(0);
 		me.input.cursor_clicked.setBoolValue(0);
 	},
+
 };
 
 var common = Common.new();

Aircraft/JA37/Nasal/displays/hud-shared.nas
@@ -28,10 +28,8 @@ var input = {
     alpha:          "/orientation/alpha-deg",
     high_alpha:     "/fdm/jsbsim/autoflight/high-alpha",
     weight:         "/fdm/jsbsim/inertia/weight-lbs",
-    alt:            "/instrumentation/altimeter/indicated-altitude-meter",
-    alt_ft:         "/instrumentation/altimeter/indicated-altitude-ft",
+    alt:            "/instrumentation/altimeter/displays-altitude-meter",
     rad_alt:        "/instrumentation/radar-altimeter/radar-altitude-m",
-    rad_alt_ft:     "/instrumentation/radar-altimeter/radar-altitude-ft",
     rad_alt_ready:  "/instrumentation/radar-altimeter/ready",
     ref_alt:        "/ja37/displays/reference-altitude-m",
     rm_active:      "/autopilot/route-manager/active",

Aircraft/JA37/Nasal/displays/ja-hud.nas
@@ -867,7 +867,9 @@ var Altitude = {
             me.group.setTranslation(380, 0);
         }
 
-        me.set_ac_alt(displays.metric ? input.alt.getValue() : input.alt_ft.getValue());
+        var ac_alt = input.alt.getValue();
+        if (!displays.metric) ac_alt *= M2FT;
+        me.set_ac_alt(ac_alt);
 
         # Markers for the linear part of the scale.
         var spacing = displays.metric ? 50 : 100;
@@ -958,7 +960,8 @@ var Altitude = {
         }
 
         if (input.rad_alt_ready.getBoolValue()) {
-            var rad_alt = displays.metric ? input.rad_alt.getValue() : input.rad_alt_ft.getValue();
+            var rad_alt = input.rad_alt.getValue();
+            if (!displays.metric) rad_alt *= M2FT;
             var pos = me.alt2pos(me.ac_alt - rad_alt);
             if (pos <= 500) {
                 me.rhm_index.setTranslation(0, pos);
@@ -1041,7 +1044,8 @@ var RadarAltitude = {
         if (modes.takeoff_30s_inhibit or modes.landing or !input.rad_alt_ready.getValue()) {
             me.shown = FALSE;
         } else {
-            var alt = displays.metric ? input.rad_alt.getValue() : input.rad_alt_ft.getValue();
+            var alt = input.rad_alt.getValue();
+            if (!displays.metric) alt *= M2FT;
             alt = math.round(alt);
             if (alt < (displays.metric ? 100 : 300)) me.shown = TRUE;
             elsif (alt >= (displays.metric ? 110 : 350)) me.shown = FALSE;

Aircraft/JA37/Nasal/displays/sight.nas
@@ -18,7 +18,7 @@ var input = {
     vel_aero_z: "/fdm/jsbsim/velocities/w-aero-fps",
     pitch_rad:  "/fdm/jsbsim/attitude/pitch-rad",
     roll_rad:   "/fdm/jsbsim/attitude/roll-rad",
-    alt:        "/instrumentation/altimeter/indicated-altitude-meter",
+    alt:        "/instrumentation/altimeter/displays-altitude-meter",
     roll:       "/instrumentation/attitude-indicator/indicated-roll-deg",
     grd_speed:  "/velocities/groundspeed-kt",
     fpv_pitch:  "/instrumentation/fpv/pitch-deg",

Aircraft/JA37/Systems/jsb-instruments.xml
@@ -225,9 +225,12 @@
             <gain>0.3048</gain>
             <output>/instrumentation/altimeter[1]/indicated-altitude-meter</output>
         </pure_gain>
-    </channel>
 
-    <channel execrate="4" name="radar-altimeter">
+        <pure_gain name="instruments/altimeter/standard-altitude-m">
+            <input>/instrumentation/altimeter/pressure-alt-ft</input>
+            <gain>0.3048</gain>
+        </pure_gain>
+
         <switch name="instruments/radar-altimeter/enabled">
             <default value="1"/>
             <test logic="OR" value="0">
@@ -262,6 +265,45 @@
                 /instrumentation/attitude-indicator/indicated-roll-deg lt -40
             </test>
             <output>/instrumentation/radar-altimeter/ready</output>
+        </switch>
+
+        <!-- Compute ground corrected altitude: If switch HÖJD CISI is in position RHM,
+            radar altitude is used to correct displayed altitude. -->
+
+        <!-- difference between radar and barometric altitude -->
+        <summer name="instruments/altimeter/ground-delta-raw-m">
+            <input>instruments/altimeter/standard-altitude-m</input>
+            <input>-instruments/radar-altimeter/radar-altitude-m</input>
+        </summer>
+
+        <switch name="instruments/altimeter/ground-delta-switched-m">
+            <!-- Use correction from radar altitude if radar altimeter is usable and switch is on RHM -->
+            <test logic="AND" value="instruments/altimeter/ground-delta-raw-m">
+                /ja37/hud/switch-hojd eq 1
+                instruments/radar-altimeter/ready eq 1
+            </test>
+            <!-- Otherwise keep previous value -->
+            <default value="instruments/altimeter/ground-delta-filtered-m"/>
+        </switch>
+
+        <lag_filter name="instruments/altimeter/ground-delta-filtered-m">
+            <input>instruments/altimeter/ground-delta-switched-m</input>
+            <c1>2</c1>
+        </lag_filter>
+
+        <!-- RHM corrected altitude -->
+        <summer name="instruments/altimeter/ground-corrected-altitude-m">
+            <input>instruments/altimeter/standard-altitude-m</input>
+            <input>-instruments/altimeter/ground-delta-filtered-m</input>
+        </summer>
+
+        <!-- Final ground corrected altitude for displays. -->
+        <switch name="instruments/altimeter/displays-altitude-m">
+            <default value="instruments/altimeter/altitude-m"/>
+            <test value="instruments/altimeter/ground-corrected-altitude-m">
+                /ja37/hud/switch-hojd eq 1
+            </test>
+            <output>/instrumentation/altimeter/displays-altitude-meter</output>
         </switch>
     </channel>
 

commit 9601586ef76ea162b3795d5f1e11ee4b6f6b899f

Displays: Fix reference altitude in landing mode
Bug introduced by 61b1dd84
Reference altitude could not be changed in landing mode.

Aircraft/JA37/Nasal/displays/common.nas
@@ -420,11 +420,11 @@ var Common = {
 				me.ref_alt += me.input.alt_m.getValue() - me.input.alt_bar_m.getValue();
 			}
 			me.ref_alt_ldg_override = FALSE;
-		} elsif (modes.landing and !me.ref_alt_ldg_override) {
+		} elsif (modes.landing) {
 			# me.ref_alt_ldg_override indicates that the altitude was manually selected
 			# with the reference altitude button while in LANDING mode.
 			# This flag is cleared in every other mode, which resets the altitude to 500 when switching to LANDING.
-			me.ref_alt = 500;
+			if (!me.ref_alt_ldg_override) me.ref_alt = 500;
 		} else {
 			# navigation mode
 			me.ref_alt_ldg_override = FALSE;